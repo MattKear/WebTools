50d6db5a03734d96b94cc2f67b57a442a774eb37
Gone4Dirt <matthewnkear@gmail.com>
2024-10-09 19:41:27 +0100

    save before some severe hacking up

--- AutorotationTool/AutorotationTool.js
+++ AutorotationTool/AutorotationTool.js
@@ -7,11 +7,12 @@ accel_plot = {}
 jerk_plot = {}
 function initial_load()
 {
-    const time_scale_label = "Time (s)"
-    let plot
+    const time_scale_label = "Time (s)";
+    let plot;
 
     // Acceleration
-    jerk_plot.data = [{ mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" }]
+    jerk_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" },
+                      { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" }]
 
     jerk_plot.layout = {
         legend: { itemclick: false, itemdoubleclick: false },
@@ -25,7 +26,8 @@ function initial_load()
     Plotly.newPlot(plot, jerk_plot.data, jerk_plot.layout, { displaylogo: false })
 
     // Acceleration
-    accel_plot.data = [{ mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" }]
+    accel_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" },
+                       { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" }]
 
     accel_plot.layout = {
         legend: { itemclick: false, itemdoubleclick: false },
@@ -34,12 +36,13 @@ function initial_load()
         yaxis: { title: {text: "Acceleration (m/s²)" } }
     }
 
-    plot = document.getElementById("accel_plot")
-    Plotly.purge(plot)
-    Plotly.newPlot(plot, accel_plot.data, accel_plot.layout, { displaylogo: false })
+    plot = document.getElementById("accel_plot");
+    Plotly.purge(plot);
+    Plotly.newPlot(plot, accel_plot.data, accel_plot.layout, { displaylogo: false });
 
     // velocity
-    vel_plot.data = [{ mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" }]
+    vel_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" },
+                     { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" }];
 
     vel_plot.layout = {
         legend: { itemclick: false, itemdoubleclick: false },
@@ -61,7 +64,8 @@ function initial_load()
     Plotly.newPlot(plot, vel_plot.data, vel_plot.layout, { displaylogo: false })
 
     // position
-    pos_plot.data = [{ mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" }]
+    pos_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" },
+                     { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" }]
 
     pos_plot.layout = {
         legend: { itemclick: false, itemdoubleclick: false },
@@ -199,15 +203,6 @@ function calc_peak_jerk_required(tj, A0, A1)
     return (A1 - A0) * 2.0 / tj;
 }
 
-function calc_seg1_jm(P0, P2, V0, V2, A0, tj)
-{
-    const t1 = P0 - P2;
-    const t2 = (V0 + V2) * tj;
-    const t3 = A0 * tj * tj * (1/3 - 1/(M_PI * M_PI));
-    const t4 = (M_PI * M_PI) / (tj * tj * tj);
-    return (t1 + t2 + t3) / t4;
-}
-
 function calculateAlpha1(P0, P2, V0, V2, A0, A2, Alpha2, tj) {
     const pi = Math.PI;
 
@@ -228,7 +223,8 @@ function fwd_project_position(P0, V0, A0, alpha1, tj)
     return P0 + 2 * V0 * tj + 2 * A0 * tj * tj + alpha1 * tj * tj *tj * (4/3 - 2 / (M_PI * M_PI))
 }
 
-function back_project_position(P0, V0, A0, Alpha, tj) {
+function back_project_position(P0, V0, A0, Alpha, tj)
+{
     const pi = Math.PI;
 
     // Calculate terms
@@ -244,6 +240,50 @@ function back_project_position(P0, V0, A0, Alpha, tj) {
     return Pt;
 }
 
+function calc_alpha2_from_peak_accel(Am, A0, tj)
+{
+    return (Am - A0) / (2 * tj)
+}
+
+function calc_alpha1_from_peak_accel(Am, A0, tj)
+{
+    return (Am - A0) / (2 * tj)
+}
+
+function calc_alpha2_halfway_conditions(A0, V0, P0, P1, tj)
+{
+    var alpha = 0
+    for (var i = 0; i<1; i++) {
+        const numerator = P1 - P0 - (2 * V0 * tj) - (2 * A0 * tj *tj);
+        const denominator = tj * tj * tj * (4/3 - 2/(M_PI*M_PI))
+        const est = numerator / denominator
+        if (alpha == 0) {
+            alpha = est;
+        } else {
+            alpha = alpha*0.8 + est*0.2;
+        }
+        // calculate the forward projection and adapt the initial connditions
+        [J1_est, A1_est, V1_est, P1_est] = calc_javp_for_segment_incr_jerk(tj*2.0, tj, alpha/2, A0, V0, P0);
+        const error = P1 - P1_est
+        P1 = P1*0.7 + P1_est*0.3
+    }
+    return alpha
+}
+
+function calculateAlpha2_from_diff(P0, P2, V0, V2, A0, A2, Alpha1, tj) {
+    const pi = Math.PI;
+
+    // Calculate the alpha factor
+    const alphaFactor = (4 / 3) - (2 / (pi * pi));
+
+    // Rearranged equation for alpha1
+    const numerator = (P0 - P2) + 2 * (V0 - V2) * tj + 2 * (A0 - A2) * tj * tj;
+    const denominator = Math.pow(tj, 3) * alphaFactor;
+
+    const Alpha2 = Alpha1 + (numerator / denominator);
+
+    return Alpha2;
+}
 
 // special handling function to adapt the enumbent s-curve maths to fit the trajectory of the autorotation
 function arot_s_curve(time_now, T, Jm, A0, V0, P0, Af)
@@ -264,6 +304,26 @@ function arot_s_curve(time_now, T, Jm, A0, V0, P0, Af)
     let JM_sec_phase = calc_peak_jerk_required(tj*2.0, A1, Af);
     let t_sec_phase = time_now - tj*2.0;
     return calc_javp_for_segment_incr_jerk(t_sec_phase, tj, JM_sec_phase, A1, V1, P1);
+}
+
+// special handling function to adapt the enumbent s-curve maths to fit the trajectory of the autorotation
+function arot_calculated_s_curve(time_now, T, A0, V0, P0, A2, V2, P2, Jm1, Jm2)
+{
+    // The 1/4 time is because S-curve definition expects the time period in a different factor to what we need in the autorotation
+    tj = T * 0.25;
+
+    // handle the positive jerk (increasing accel in the first half of the flare time)
+    if (time_now <= tj*2.0) {
+        return calc_javp_for_segment_incr_jerk(time_now, tj, Jm1, A0, V0, P0);
+    }
+
+    // if we got this far then we are doing the negative jerk portion of the trajectory
+    // first we need to calculate the initial conditions of the negative trajectory, these are the exit conditions of the positive jerk trajectory
+    let [J1, A1, V1, P1] = calc_javp_for_segment_incr_jerk(tj*2.0, tj, Jm1, A0, V0, P0);
+
+    let t_sec_phase = time_now - tj*2.0;
+
+    return calc_javp_for_segment_incr_jerk(t_sec_phase, tj, Jm2, A2+A1, V1, P1);
 }
 
 // Calculate the jerk, acceleration, velocity and position at time time_now when running the increasing jerk magnitude time segment based on a raised cosine profile
@@ -316,7 +376,7 @@ function run_flare()
 {
     const A0 = parseFloat(document.getElementById("inital_accel").value);
     const V0 = parseFloat(document.getElementById("initial_vel").value);
-    const P0 = parseFloat(document.getElementById("initial_pos").value);
+    var P0 = parseFloat(document.getElementById("initial_pos").value);
 
     const A2 = parseFloat(document.getElementById("final_accel").value);
     const V2 = parseFloat(document.getElementById("final_vel").value);
@@ -324,26 +384,64 @@ function run_flare()
 
     const Jm = parseFloat(document.getElementById("max_jerk").value);
     const T = parseFloat(document.getElementById("flare_time").value);
+    const AZm = parseFloat(document.getElementById("max_vert_accel").value);
 
-    const jm_seg1_est = calc_seg1_jm(P0, P2, V0, V2, A0, T*0.25);
-    console.log(jm_seg1_est);
+    // var pred_P1 = fwd_project_position(P0, V0, A0, 13.0/2.0, T*0.25) // This matches
+    // console.log(`Predicted P1 = ${pred_P1} m`);
 
-    console.log(`P0 = ${P0}`);
-    console.log(`P2 = ${P2}`);
-    console.log(`V0 = ${V0}`);
-    console.log(`V2 = ${V2}`);
-
-    var pred_P1 = fwd_project_position(P0, V0, A0, 13.0/2.0, T*0.25) // This matches
-    console.log(`Predicted P1 = ${pred_P1} m`);
-
-    let [J1_pred, A1_pred, V1_pred, P1_pred] = calc_javp_for_segment_incr_jerk(T*0.5, T*0.25, 19, A2, V2, P2) // --- this is giving the correct answer
-    console.log(`javp Predicted P1 = ${P1_pred+0.07599} m`);
-
-
+    // let [J1_pred, A1_pred, V1_pred, P1_pred] = calc_javp_for_segment_incr_jerk(T*0.5, T*0.25, 19, A2, V2, P2) // --- this is giving the correct answer
+    // console.log(`javp Predicted P1 = ${P1_pred+0.07599} m`);
 
     // init a time vector
     const t = linspace(0.0, T, 1000);
 
+
+
+    let alpha2_pred = calc_alpha2_from_peak_accel(AZm, A2, T*0.25);
+    let jm2 = alpha2_pred*2.0;
+    console.log(`Jm 2 Predicition = ${jm2.toFixed(4)} (m/s³)`);
+
+    var alpha1_pred = calculateAlpha1(P0, P2, V0, V2, A0, A2, alpha2_pred, T*0.25) // <------ this works!!!!!!!!!!!!, fuck yeah!
+    var jm1 = alpha1_pred*2
+    console.log(`Jm 1 Predicition = ${jm1.toFixed(4)} (m/s³)`);
+
+    // V2 calculation
+    // use the max accel available to apply the breaks
+    const alpha_1_v2 = calc_alpha1_from_peak_accel(AZm, A0, T*0.25)
+    const jm1_v2 = alpha_1_v2*2.0;
+    console.log(`Jm 1 V2 Predicition = ${jm1_v2.toFixed(4)} (m/s³)`);
+
+    // now calculate the alpha2 required to meet my requested exit conditions
+    let [J1_v2, A1_v2, V1_v2, P1_v2] = calc_javp_for_segment_incr_jerk(T*0.5, T*0.25, jm1_v2, A0, V0, P0);
+    const alpha_2_v2 = calc_alpha2_halfway_conditions(A2, V2, P2, P1_v2, -T*0.25);
+    // const alpha_2_v2 = calc_alpha2_halfway_conditions(A1_v2, V1_v2, P1_v2, P2, T*0.25);
+    // const alpha_2_v2 = calc_alpha1_from_peak_accel(A2, AZm, T*0.25)
+    const jm2_v2 = alpha_2_v2*2.0;
+    console.log(`Jm 2 V2 Predicition = ${jm2_v2.toFixed(4)} (m/s³)`);
+
+    // we know how far the vehicle will travel, so we will specify the initiating position based on 
+    // the final desired position and the projected distance to be traveled
+    let [J2_v2, A2_v2, V2_v2, P2_v2] = calc_javp_for_segment_incr_jerk(T*0.5, T*0.25, jm2_v2, A1_v2, V1_v2, P1_v2)
+    // update the initial position
+    P0 = P0 - P2_v2
+    console.log(P1_v2)
+    console.log(P2_v2)
+    document.getElementById("initial_pos").value = P0
+
+    // updated method that calculates the neccassary jerk peaks to achieve the entry and exit conditions
+    // "smart" method
+    var calcd_traj = new Trajectory();
+    for (var i = 0; i < t.length; i++) {
+        // calculate the variables for the trajectory
+        // const [Jt, At, Vt, Pt] = arot_calculated_s_curve(t[i], T, A0, V0, P0, A2, V2, P2, jm1, -jm2);
+        const [Jt, At, Vt, Pt] = arot_calculated_s_curve(t[i], T, A0, V0, P0, A2, V2, P2, jm1_v2, jm2_v2);
+        calcd_traj.j.push(Jt);
+        calcd_traj.a.push(At);
+        calcd_traj.v.push(Vt);
+        calcd_traj.p.push(Pt);
+    }
+
+    // dumb original method
     var traj = new Trajectory();
     for (var i = 0; i < t.length; i++) {
         // calculate the variables for the trajectory
@@ -354,25 +452,29 @@ function run_flare()
         traj.p.push(Pt);
     }
 
-    var gpt_alpha1 = calculateAlpha1(P0, P2+traj.p[traj.p.length-1], V0, V2, A0, A2, 19/2, T*0.25) // <------ this works!!!!!!!!!!!!, fuck yeah!
-    var jm2 = gpt_alpha1*2
-    console.log(`Jm 1 Predicition = ${jm2}`);
-
     // Update plots
     jerk_plot.data[0].x = t
     jerk_plot.data[0].y = traj.j
+    jerk_plot.data[1].x = t
+    jerk_plot.data[1].y = calcd_traj.j
     Plotly.redraw("jerk_plot")
 
     accel_plot.data[0].x = t
     accel_plot.data[0].y = traj.a
+    accel_plot.data[1].x = t
+    accel_plot.data[1].y = calcd_traj.a
     Plotly.redraw("accel_plot")
 
     vel_plot.data[0].x = t
     vel_plot.data[0].y = traj.v
+    vel_plot.data[1].x = t
+    vel_plot.data[1].y = calcd_traj.v
     Plotly.redraw("vel_plot")
 
     pos_plot.data[0].x = t
     pos_plot.data[0].y = traj.p
+    pos_plot.data[1].x = t
+    pos_plot.data[1].y = calcd_traj.p
     Plotly.redraw("pos_plot")
 
 }
