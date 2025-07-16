929ddb280a57434dd5c32ca42ce6da020f27fb55
MattKear <matthewnkear@gmail.com>
2025-07-16 15:18:11 +0100

    Autorotation: Clean up and simulate initial descending kinematics

--- AutorotationTool/AutorotationTool.js
+++ AutorotationTool/AutorotationTool.js
@@ -10,12 +10,12 @@ function initial_load()
     const time_scale_label = "Time (s)";
     let plot;
 
-    // Acceleration
-    jerk_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" },
-                      { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" }]
+    // Jerk
+    jerk_plot.data = [{ x:[], y:[], name: 'Flare Start', mode: 'lines', line: {dash: 'dash'}, hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" },
+                      { x:[], y:[], name: 'Trajectory', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s³" }]
 
     jerk_plot.layout = {
-        legend: { itemclick: false, itemdoubleclick: false },
+        legend: { itemclick: false, itemdoubleclick: false, x: 0.8 },
         margin: { b: 50, l: 60, r: 50, t: 20 },
         xaxis: { title: {text: time_scale_label } },
         yaxis: { title: {text: "Jerk (m/s³)" } }
@@ -26,11 +26,11 @@ function initial_load()
     Plotly.newPlot(plot, jerk_plot.data, jerk_plot.layout, { displaylogo: false })
 
     // Acceleration
-    accel_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" },
-                       { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" }]
+    accel_plot.data = [{ x:[], y:[], name: 'Flare Start', mode: 'lines', line: {dash: 'dash'}, hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" },
+                       { x:[], y:[], name: 'Trajectory', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s²" }]
 
     accel_plot.layout = {
-        legend: { itemclick: false, itemdoubleclick: false },
+        legend: { itemclick: false, itemdoubleclick: false, x: 0.8 },
         margin: { b: 50, l: 60, r: 50, t: 20 },
         xaxis: { title: {text: time_scale_label } },
         yaxis: { title: {text: "Acceleration (m/s²)" } }
@@ -41,11 +41,11 @@ function initial_load()
     Plotly.newPlot(plot, accel_plot.data, accel_plot.layout, { displaylogo: false });
 
     // velocity
-    vel_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" },
-                     { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" }];
+    vel_plot.data = [{ x:[], y:[], name: 'Flare Start', mode: 'lines', line: {dash: 'dash'}, hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" },
+                     { x:[], y:[], name: 'Trajectory', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m/s" }];
 
     vel_plot.layout = {
-        legend: { itemclick: false, itemdoubleclick: false },
+        legend: { itemclick: false, itemdoubleclick: false, x: 0.8 },
         margin: { b: 50, l: 60, r: 50, t: 20 },
         xaxis: { title: {text: time_scale_label } },
         yaxis: { title: {text: "Velocity (m/s)" } },
@@ -64,11 +64,12 @@ function initial_load()
     Plotly.newPlot(plot, vel_plot.data, vel_plot.layout, { displaylogo: false })
 
     // position
-    pos_plot.data = [{ x:[], y:[], name: 'Dumb', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" },
-                     { x:[], y:[], name: 'Smart', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" }]
+    pos_plot.data = [{ x:[], y:[], name: 'Flare Start', mode: 'lines', line: {dash: 'dash'}, hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" },
+                     { x:[], y:[], name: 'Trajectory', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" },
+                    { x:[], y:[], name: 'Look Forward', mode: 'lines', hovertemplate: "<extra></extra>%{x:.2f} s<br>%{y:.2f} m" }]
 
     pos_plot.layout = {
-        legend: { itemclick: false, itemdoubleclick: false },
+        legend: { itemclick: false, itemdoubleclick: false, x: 0.8},
         margin: { b: 50, l: 60, r: 50, t: 20 },
         xaxis: { title: {text: time_scale_label } },
         yaxis: { title: {text: "Position (m)" } },
@@ -203,89 +204,6 @@ function calc_peak_jerk_required(tj, A0, A1)
     return (A1 - A0) * 2.0 / tj;
 }
 
-function calculateAlpha1(P0, P2, V0, V2, A0, A2, Alpha2, tj) {
-    const pi = Math.PI;
-
-    // Calculate the alpha factor
-    const alphaFactor = (4 / 3) - (2 / (pi * pi));
-
-    // Rearranged equation for alpha1
-    const numerator = -(P0 - P2) - 2 * (V0 - V2) * tj - 2 * (A0 - A2) * tj * tj;
-    const denominator = Math.pow(tj, 3) * alphaFactor;
-
-    const Alpha1 = Alpha2 + (numerator / denominator);
-
-    return Alpha1;
-}
-
-function fwd_project_position(P0, V0, A0, alpha1, tj)
-{
-    return P0 + 2 * V0 * tj + 2 * A0 * tj * tj + alpha1 * tj * tj *tj * (4/3 - 2 / (M_PI * M_PI))
-}
-
-function back_project_position(P0, V0, A0, Alpha, tj)
-{
-    const pi = Math.PI;
-
-    // Calculate terms
-    const term1 = P0;
-    const term2 = 2 * V0 * tj;
-    const term3 = 2 * A0 * tj * tj;
-    const term4 = (-2 * Alpha * tj) / (pi * pi);
-    const term5 = Alpha * tj * tj * tj * (4 / 3 - 2 / (pi * pi));
-
-    // Calculate the final position
-    const Pt = term1 + term2 + term3 + term4 + term5;
-
-    return Pt;
-}
-
-function calc_alpha2_from_peak_accel(Am, A0, tj)
-{
-    return (Am - A0) / (2 * tj)
-}
-
-function calc_alpha1_from_peak_accel(Am, A0, tj)
-{
-    return (Am - A0) / (2 * tj)
-}
-
-function calc_alpha2_halfway_conditions(A0, V0, P0, P1, tj)
-{
-    var alpha = 0
-    for (var i = 0; i<1; i++) {
-        const numerator = P1 - P0 - (2 * V0 * tj) - (2 * A0 * tj *tj);
-        const denominator = tj * tj * tj * (4/3 - 2/(M_PI*M_PI))
-        const est = numerator / denominator
-        if (alpha == 0) {
-            alpha = est;
-        } else {
-            alpha = alpha*0.8 + est*0.2;
-        }
-        // calculate the forward projection and adapt the initial connditions
-        [J1_est, A1_est, V1_est, P1_est] = calc_javp_for_segment_incr_jerk(tj*2.0, tj, alpha/2, A0, V0, P0);
-        const error = P1 - P1_est
-        P1 = P1*0.7 + P1_est*0.3
-    }
-    return alpha
-}
-
-
-function calculateAlpha2_from_diff(P0, P2, V0, V2, A0, A2, Alpha1, tj) {
-    const pi = Math.PI;
-
-    // Calculate the alpha factor
-    const alphaFactor = (4 / 3) - (2 / (pi * pi));
-
-    // Rearranged equation for alpha1
-    const numerator = (P0 - P2) + 2 * (V0 - V2) * tj + 2 * (A0 - A2) * tj * tj;
-    const denominator = Math.pow(tj, 3) * alphaFactor;
-
-    const Alpha2 = Alpha1 + (numerator / denominator);
-
-    return Alpha2;
-}
-
 // special handling function to adapt the enumbent s-curve maths to fit the trajectory of the autorotation
 function arot_s_curve(time_now, T, Jm, A0, V0, P0, Af)
 {
@@ -365,76 +283,158 @@ class Trajectory
 
 function run_flare()
 {
-    const A0 = parseFloat(document.getElementById("inital_accel").value);
+
+    const rotor_rad = parseFloat(document.getElementById("rotor_radius").value);
+    const rotor_cd = parseFloat(document.getElementById("rotor_cd").value);
+    const mass = parseFloat(document.getElementById("mass").value);
+
+    // const A0 = parseFloat(document.getElementById("inital_accel").value);
     const V0 = parseFloat(document.getElementById("initial_vel").value);
-    var P0 = parseFloat(document.getElementById("initial_pos").value);
+    const P0 = parseFloat(document.getElementById("initial_pos").value);
 
     const A2 = parseFloat(document.getElementById("final_accel").value);
     const V2 = parseFloat(document.getElementById("final_vel").value);
     const P2 = parseFloat(document.getElementById("final_pos").value);
 
     const Jm = parseFloat(document.getElementById("max_jerk").value);
-    const T = parseFloat(document.getElementById("flare_time").value);
-    const AZm = parseFloat(document.getElementById("max_vert_accel").value);
+    // const T = parseFloat(document.getElementById("flare_time").value);
+    // const AZm = parseFloat(document.getElementById("max_vert_accel").value);
+
+    const density = 1.225; // (kg/m^3)
+    const gravity = -9.81; // (m/s/s)
+    const rotor_area = M_PI * rotor_rad * rotor_rad; // (m^2)
+    const rotor_drag = 0.5 * density * rotor_area * rotor_cd // (kg/s)
 
 
-    console.log(`Inputs:\njm = ${Jm}\na0 = ${A0}\na2 = ${A2}\nv0 = ${V0}\nv2 = ${V2}`)
+
+    // console.log(`Inputs:\njm = ${Jm}\na0 = ${A0}\na2 = ${A2}\nv0 = ${V0}\nv2 = ${V2}`)
 
     // "smart" method
     // variable time periods to compute the necessary trajectory
-    const [tj1, tj2] = compute_time_split(Jm, A0, A2, V0, V2)
-    const total_time = (tj1+tj2)*2.0
-    console.log(`Calculated Time Splits:\nT1 = ${tj1*2.0},\nT2 = ${tj2*2.0},\nTotal T = ${total_time}`)
+    // const [tj1, tj2] = compute_time_split(Jm, A0, A2, V0, V2)
+    // const total_time = (tj1+tj2)*2.0
+    // console.log(`Calculated Time Splits:\nT1 = ${tj1*2.0},\nT2 = ${tj2*2.0},\nTotal T = ${total_time}`)
 
 
     // init a time vector
-    const t = linspace(0.0, Math.max(T, total_time), 1000);
+    const dt = 0.01 // (s)
+    let t = 0.0
+    const time = []
 
     var calcd_traj = new Trajectory();
-    for (var i = 0; i < t.length; i++) {
-        // const sampleT = 0.5*(T1+T2);
-        const [Jt, At, Vt, Pt] = arot_calculated_s_curve(t[i], tj1, tj2, A0, V0, P0, Jm);
+    let Jt = 0.0;
+    let At = 0.0;
+    let Vt = V0;
+    let Pt = P0;
 
+    let in_flare = false;
+    let flare_finished = false;
+    let flare_init = {t:0.0, a:0.0, v:0.0, p:0.0};
+    let tj1, tj2;
+
+    let P_end_hist = [];
+
+    // Run simulation
+    while (t < 100.0) {
+
+        if (!in_flare) {
+            // Crude simulation of heli descending in glide
+            const weight = mass * gravity; // (N)
+            const drag_direction = Math.sign(Vt) * -1.0; // drag always works in the opposite direction to velocity
+            const drag_force = rotor_drag * Vt * Vt * drag_direction; // (N)
+            const resultant_force = drag_force + weight; // (N)
+
+            // Assume constant accel/zero jerk
+            Jt = 0.0;
+            At = resultant_force / mass;
+            const initial_V = Vt;
+            Vt = initial_V + At * dt;
+            Pt += initial_V * dt + 0.5 * At * dt * dt;
+
+
+            // Calculate the s-curve trajectory look forward position
+            [tj1, tj2] = compute_time_split(Jm, At, A2, Vt, V2)
+            const T_end = (tj1 + tj2) * 2.0;
+            const [, , , P_end] = arot_calculated_s_curve(T_end, tj1, tj2, At, Vt, Pt, Jm);
+            P_end_hist.push(P_end);
+
+            in_flare = P_end <= P2;
+            // keep flare init up to date
+            flare_init.t = t;
+            flare_init.a = At;
+            flare_init.v = Vt;
+            flare_init.p = Pt;
+
+        } else if (!flare_finished) {
+            const flare_time = t - flare_init.t;
+            [Jt, At, Vt, Pt] = arot_calculated_s_curve(flare_time, tj1, tj2, flare_init.a, flare_init.v, flare_init.p, Jm);
+
+            // Add values to keep array length correct
+            P_end_hist.push(P2);
+
+            flare_finished = t >= flare_init.t + (tj1 + tj2) * 2.0
+        } else {
+
+            // Assume constant accel at exit condition (not updating accel and jerk)
+            const initial_V = Vt;
+            Vt = initial_V + At * dt;
+            Pt += initial_V * dt + 0.5 * At * dt * dt;
+        }
+
+
+        time.push(t)
         calcd_traj.j.push(Jt);
         calcd_traj.a.push(At);
         calcd_traj.v.push(Vt);
         calcd_traj.p.push(Pt);
+
+        // Break from simulation
+        if (flare_finished && Pt <= 0) {
+            break;
+        }
+
+        // update time for the next time step
+        t += dt; 
     }
 
-    // dumb original method
-    var traj = new Trajectory();
-    for (var i = 0; i < t.length; i++) {
-        // calculate the variables for the trajectory
-        const [Jt, At, Vt, Pt] = arot_s_curve(t[i], T, Jm, A0, V0, P0, A2);
-        traj.j.push(Jt);
-        traj.a.push(At);
-        traj.v.push(Vt);
-        traj.p.push(Pt);
-    }
+    console.log(P_end_hist)
+
+    // // dumb original method
+    // var traj = new Trajectory();
+    // for (var i = 0; i < t.length; i++) {
+    //     // calculate the variables for the trajectory
+    //     const [Jt, At, Vt, Pt] = arot_s_curve(t[i], T, Jm, A0, V0, P0, A2);
+    //     traj.j.push(Jt);
+    //     traj.a.push(At);
+    //     traj.v.push(Vt);
+    //     traj.p.push(Pt);
+    // }
 
     // Update plots
-    jerk_plot.data[0].x = t
-    jerk_plot.data[0].y = traj.j
-    jerk_plot.data[1].x = t
+    jerk_plot.data[0].x = [flare_init.t, flare_init.t]
+    jerk_plot.data[0].y = [Math.min(...calcd_traj.j), Math.max(...calcd_traj.j)]
+    jerk_plot.data[1].x = time
     jerk_plot.data[1].y = calcd_traj.j
     Plotly.redraw("jerk_plot")
 
-    accel_plot.data[0].x = t
-    accel_plot.data[0].y = traj.a
-    accel_plot.data[1].x = t
+    accel_plot.data[0].x = [flare_init.t, flare_init.t]
+    accel_plot.data[0].y = [Math.min(...calcd_traj.a), Math.max(...calcd_traj.a)]
+    accel_plot.data[1].x = time
     accel_plot.data[1].y = calcd_traj.a
     Plotly.redraw("accel_plot")
 
-    vel_plot.data[0].x = t
-    vel_plot.data[0].y = traj.v
-    vel_plot.data[1].x = t
+    vel_plot.data[0].x = [flare_init.t, flare_init.t]
+    vel_plot.data[0].y = [Math.min(...calcd_traj.v), Math.max(...calcd_traj.v)]
+    vel_plot.data[1].x = time
     vel_plot.data[1].y = calcd_traj.v
     Plotly.redraw("vel_plot")
 
-    pos_plot.data[0].x = t
-    pos_plot.data[0].y = traj.p
-    pos_plot.data[1].x = t
+    pos_plot.data[0].x = [flare_init.t, flare_init.t]
+    pos_plot.data[0].y = [Math.min(...calcd_traj.p), Math.max(...calcd_traj.p)]
+    pos_plot.data[1].x = time
     pos_plot.data[1].y = calcd_traj.p
+    pos_plot.data[2].x = time
+    pos_plot.data[2].y = P_end_hist
     Plotly.redraw("pos_plot")
 
 }
